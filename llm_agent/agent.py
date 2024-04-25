a75a831540c11ef59ffb105e8d994a92b1951cb1
Kostya Pershin <kostya.pershin.18@mail.ru>
2024-04-25 18:22:32 +0500

    update prompt

--- llm_agent/agent.py
+++ llm_agent/agent.py
@@ -4,34 +4,17 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.agents import AgentExecutor, create_gigachat_functions_agent
 from langchain_community.chat_models.gigachat import GigaChat
 from llm_agent.retrieval_tool import get_retrieval_tool
+from langchain_core.tools import tool
 from langchain import hub
 
 
 def get_agent_response(user_query: str):
     giga = GigaChat(credentials=AU_DATA, verify_ssl_certs=False, scope="GIGACHAT_API_CORP")
 
-    retriever_tool = get_retrieval_tool('https://mc21.ru/blog/shchitovidka-i-zuby-kakaya-svyaz-mezhdu-patologiey-polosti-rta-i-shchitovidnoy-zhelezy/')
-    tools = [retriever_tool]
+    @tool
+    def multiply(first_int: int, second_int: int) -> int:
+        """Multiply two integers together."""
+        return first_int * second_int
 
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "Ты полезный ассистент, который помогает найти ответы на вопросы",
-            ),
-            ("user", "{input}"),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
-        ]
-    )
-
-    agent = create_gigachat_functions_agent(giga, tools, prompt)
-
-    agent_executor = AgentExecutor(
-        agent=agent,
-        tools=tools,
-        verbose=True,
-    )
-
-    result = agent_executor.invoke({"input": user_query})["output"]
-
-    return result
+    llm_with_tools = giga.bind_tools([multiply])
+    msg = llm_with_tools.invoke("whats 5 times forty two")
