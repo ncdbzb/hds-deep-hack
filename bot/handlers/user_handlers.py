c1e9e2cc878c377375cf685ff026f43b01063955
Kostya Pershin <kostya.pershin.18@mail.ru>
2024-04-26 11:02:08 +0500

    add user query validator

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,8 +1,9 @@
+import re
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.filters import Command, CommandStart, StateFilter
+from aiogram.filters import CommandStart, StateFilter
 from bot.lexicon.lexicon_ru import LEXICON_RU
 from llm_agent.agent import Bibliography
 
@@ -19,7 +20,10 @@ async def process_start_command(message: Message, state: FSMContext):
     await state.set_state(FSMGame.search_message)
 
 
-@router.message(StateFilter(FSMGame.search_message))
+@router.message(StateFilter(FSMGame.search_message), F.text)
 async def process_llm_message(message: Message):
-    response = Bibliography(message.text).execute_agent()
-    await message.answer(text=response)
+    if re.search(r'https?://\S+', message.text):
+        response = Bibliography(message.text).execute_agent()
+        await message.answer(text=response)
+    else:
+        await message.answer(text=LEXICON_RU['error_message'])
